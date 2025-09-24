38f2f813d642fa1fedc91b18b95aa30d5fe68031
BsHeR4 <bshermahayni@gmail.com>
2025-09-24 13:29:11 +0300

    finish accordion

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,17 +1,25 @@
-import Container from '../Container/Container'
+import { useState } from 'react';
 import IconButton from '../IconButton/IconButton';
 import styles from './Accordion.module.css'
 import { LuPlus, LuMinus } from "react-icons/lu";
 
 const Accordion = ({ title, content, active = false }) => {
+    const [isActive, setIsActive] = useState(false);
+
+    const openAccordion = () => {
+        setIsActive(!isActive);
+    }
+
     return (
-        <div className={`${styles.accordion} ${active ? styles.active : ''}`}>
+        <div className={`${styles.accordion} ${isActive ? styles.active : ''}`}>
             <div className={styles.accordionHeader}>
                 <h3>{title}</h3>
-                <IconButton icon={active ? <LuMinus /> : <LuPlus />} className={styles.iconButton} />
+                <IconButton onClick={openAccordion} icon={isActive ? <LuMinus /> : <LuPlus />} className={styles.iconButton} />
             </div>
             <div className={styles.accordionContent}>
-                <p>{content}</p>
+                <div className={styles.contentWrapper}>
+                    <p>{content}</p>
+                </div>
             </div>
         </div>
     )
